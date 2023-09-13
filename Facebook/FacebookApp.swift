d0c80ab03750932404d16c73121d4c42effed95a
Pedro <pferezin@magnasistemas.com.br>
2023-09-13 20:24:15 -0300

    Initial Commit

---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Pedro Henrique on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
